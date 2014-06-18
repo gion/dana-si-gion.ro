d68a445bd67c95e8b41a63527a019394a3816f58
bogdan gradinariu <bogdan.gradinariu@xivic.com>
2014-06-18 22:25:00 +0300

    added yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name danaSiGionroApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the danaSiGionroApp
+ */
+angular.module('danaSiGionroApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
