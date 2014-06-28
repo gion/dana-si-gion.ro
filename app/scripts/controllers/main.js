8e6eb940075edc8b76386af78de8186ee937d792
bogdan gradinariu <bogdan.gradinariu@xivic.com>
2014-06-28 20:55:15 +0300

    v 1.0.0

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,27 +8,62 @@
  * Controller of the danaSiGionroApp
  */
 angular.module('danaSiGionroApp')
-  .controller('MainCtrl', ['$scope', '$rootScope', 'config', '$firebase', '$timeout',
-  	function ($scope, $rootScope, config, $firebase, $timeout) {
-		//$firebase
-		var ref = new Firebase(config.fireBaseUrl + '/registered-users');
-		$scope.registeredUsers = $firebase(ref);
+	.controller('MainCtrl', ['$scope', '$rootScope', 'config', '$firebase', '$timeout',
+	  	function ($scope, $rootScope, config, $firebase, $timeout) {
+			var mouseMoveTimeout;
 
-		// trigger the stamp-shake animation
-		$timeout(function() {
-			$rootScope.shakeIt = true;
-		}, 1000);
+			//$firebase
+			var ref = new Firebase(config.fireBaseUrl + '/registered-users');
+			$scope.registeredUsers = $firebase(ref);
 
-		var mouseMoveTimeout;
 
-		$scope.mouseMove = function() {
-			console.log('da');
-			$timeout.cancel(mouseMoveTimeout);
-			$scope.showOverlay = true;
-			mouseMoveTimeout = $timeout(function() {
-				console.log('nu');
-				$scope.showOverlay = false;
-			}, 3000);
+			$scope.mouseMove = function() {
+				$timeout.cancel(mouseMoveTimeout);
+				$scope.showOverlay = true;
+				mouseMoveTimeout = $timeout(function() {
+					// $scope.showOverlay = false;
+				}, 3000);
+			};
+
+			$scope.goTo = function($event, index) {
+				// alert(index);
+				$event.preventDefault();
+				$event.stopPropagation();
+
+				$scope.currentFieldIndex = Math.min(4, Math.max(1, index));
+				return false;
+			};
+
+			$scope.keypress = function($event) {
+				var tabPressed = $event.which == 9,
+					shiftPressed = $event.shiftKey,
+					newIndex;
+				
+				if(!tabPressed) {
+					return;
+				}
+
+				if(shiftPressed) {
+			        newIndex = $scope.currentFieldIndex - 1;
+			    } else {
+			        newIndex = $scope.currentFieldIndex + 1;
+			    }
+
+			    $scope.goTo($event, newIndex);
+			};
+
+			$scope.submit = function() {
+				$scope.registeredUsers
+					.$add({
+						name: $scope.form.name,
+						email: $scope.form.email,
+						couple: !!$scope.form.couple
+					})
+					.then(function() {
+						$scope.thanks = true;
+					});
+
+				// $scope.form = {};
+			}
 		}
-	  }
-  ]);
+ 	]);
