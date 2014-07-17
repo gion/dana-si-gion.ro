c7465bf3584267f57090ae0707da5aa3b8e76a9e
gion <bogdan.gradinariu@xivic.com>
2014-07-17 14:44:05 +0300

    freshly backed yeoman angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('danaSiGionApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
