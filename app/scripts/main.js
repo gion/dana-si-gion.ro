bf6a7242fa19bc4ac5bf24534cd29887208c7ed9
gion <bogdan.gradinariu@xivic.com>
2014-07-18 18:28:50 +0300

    major stuff going on:  \n - overall scene structure completed  \n - added description  \n - added sighisoara castle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@
 (function (window, api, $, TweenMax, ScrollMagic, ScrollScene, Modernizr, IScroll) {
 
   $(document).ready(function ($) {
+    // return;
 
     $('#bullets').on('click', '.bullet', function (e) {
       var index = $(e.target).index();
@@ -15,7 +16,7 @@
       if(api.animateScroll.tween) {
         api.animateScroll.tween.kill();
       }
-      // console.log('animate scroll', top);
+      console.log('animate scroll', top);
 
       var scrollContainer = $('.scrollContainer').get(0),
         curScroll = {
@@ -50,10 +51,10 @@
     var controller = api.controller = new ScrollMagic({container: '#example-wrapper'});
 
     // init tween
-    var tween = TweenMax.to('#mobileadvanced #hill', 1, {rotation: -10 * 360 + 'deg'});
+    var tween = TweenMax.to('#mobileadvanced #hill', 1, {rotation: -2 * 360 + 'deg'});
 
     // init scene
-    var scene = api.backgroundScene = new ScrollScene({triggerElement: '#background', duration: $('.scrollContent').height()})
+    var scene = api.backgroundScene = new ScrollScene({triggerElement: $('#background').get(0), duration: $('.scrollContent').height()})
             .setTween(tween)
             .setPin('#mobileadvanced', {pushFollowers: false})
             .addTo(controller);
@@ -61,13 +62,13 @@
     scene.on('enter', function (event) {
       $('body').removeClass('finished');
       $(scene.triggerElement()).addClass('active');
-      console.log('enter', event, scene);
+      console.log('enter', scene.triggerElement());
     });
 
     scene.on('leave', function (event) {
       $(scene.triggerElement()).removeClass('active');
       $('body').addClass('finished');
-      console.log('leave', event, scene);
+      console.info('leave', scene.triggerElement());
     });
 
     // scenes.push(scene);
@@ -82,20 +83,20 @@
           .setPin(el, {pushFollowers: false})
           .setTween(
             TweenMax.fromTo(sceneElement, 1,
-              {rotation: '-135deg'},
-              {rotation: '135deg'}
+              {rotation: '-125deg'},
+              {rotation: '125deg'}
             )
           );
       });
 
       s.on('enter', function (event) {
         $(s.triggerElement()).addClass('active');
-        console.log('enter', event, s);
+        console.log('enter', s.triggerElement());
       });
 
       s.on('leave', function (event) {
         $(s.triggerElement()).removeClass('active');
-        console.log('leave', event, s);
+        console.info('leave', s.triggerElement());
       });
 
       // scenes.push(s);
@@ -105,37 +106,59 @@
 
     });
 
+    // actual scenes here
     $('.scene:not(.fixed)').each(function(i, sceneEl) {
-      var s =  new ScrollScene({triggerElement: sceneEl, duration: $(sceneEl).data('duration') || $(sceneEl).height(), offset: -200});
+      var s = new ScrollScene({
+            triggerElement: sceneEl,
+            offset: -200,
+            duration: $(sceneEl).data('duration') || $(sceneEl).height()
+          }),
+          timeline = new TimelineMax();
 
-      $('.scene-element-wrapper', sceneEl).each(function(i, el) {
-        var sceneElement = $('.scene-element', el).get(0);
+       // for scene element (wrapper)
+      $('.scene-element-wrapper', sceneEl).each(function(j, el) {
+        console.log(j, el);
+        // get the element
+        var sceneElement = $('.scene-element', el).get(0),
+            descriptionElement = $('.scene-description', el),
+            tween = TweenMax.fromTo(sceneElement, 1,
+              // {rotation: '135deg'},
+              // {rotation: '-45deg'}
+              {rotation: '135deg'},
+              {
+                rotation: '-120deg',
+                onStart: function() {
+                  // descriptionElement.show();
+                },
+                onComplete: function() {
+                  // descriptionElement.hide();
+                }
+              }
+            );
 
-        s
-          .setPin(el, {pushFollowers: false})
-          .setTween(
-            TweenMax.fromTo(sceneElement, 1,
-              {rotation: '90deg'},
-              {rotation: '-90deg'}
-            )
-          );
+        timeline.add(tween);
       });
+
+      window.timeline = timeline;
 
       s.on('enter', function (event) {
         $(s.triggerElement()).addClass('active');
         var index = scenes.indexOf(s);
         $('#bullets .bullet').removeClass('active').eq(index).addClass('active');
-        console.log('enter', event, s);
+        console.log('enter', s.triggerElement());
       });
 
       s.on('leave', function (event) {
         $(s.triggerElement()).removeClass('active');
-        console.log('leave', event, s);
+        console.info('leave', s.triggerElement());
       });
 
-      scenes.push(s);
+      s.setTween(timeline);
+      // s.setPin(sceneEl/*, {pushFollowers: true}*/);
       s.addTo(controller);
+      s.addIndicators();
 
+      scenes.push(s);
     });
 
     // var firstScene = new ScrollScene({triggerElement: '#scene-1', duration: $('#scene-1').height(), offset: 0})
