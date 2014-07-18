478d63065518d24aade8744dae5313570ea095ee
gion <bogdan.gradinariu@xivic.com>
2014-07-18 13:18:48 +0300

    made gh pages push in MY Name!

--- Gruntfile.js
+++ Gruntfile.js
@@ -340,13 +340,17 @@ module.exports = function (grunt) {
     'gh-pages': {
       options: {
         base: 'dist',
-        message: process.env.CI_MESSAGE + '\n => ' + process.env.CI_COMMIT_ID + '\n auto-generated commit --skip-ci',
+        message: 'CI:' + process.env.CI_MESSAGE + '\n => ' + process.env.CI_COMMIT_ID + '\n auto-generated commit --skip-ci',
         // silent: true,
         repo: 'https://' + process.env.GH_TOKEN + '@github.com/gion/dana-si-gion.ro.git',
         user: {
-          name: process.env.CI_COMMITTER_USERNAME,
-          email: process.env.CI_COMMITTER_EMAIL
+          name: 'gion',
+          email: 'bogdan.gradinariu@gmail.com'
         }
+        // user: {
+        //   name: process.env.CI_COMMITTER_USERNAME,
+        //   email: process.env.CI_COMMITTER_EMAIL
+        // }
       },
       src: ['**']
     }
